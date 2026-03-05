033dfad7057b55a6006cd2c470962a8e0efa3015
Victor Ndifon <ndifonvictor4@gmail.com>
2026-03-05 17:16:24 +0100

    page zoom fix

--- src/components/ecommerce/CartDrawer.jsx
+++ src/components/ecommerce/CartDrawer.jsx
@@ -47,7 +47,7 @@ export const CartDrawer = () => {
             <ul className="divide-y divide-gray-200">
               {items.map((item) => (
                 <li key={item.variant.id} className="py-6 flex">
-                  <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200 bg-gray-100">
+                  <div className="h-24 w-24 shrink-0 overflow-hidden rounded-md border border-gray-200 bg-gray-100">
                     <img
                       src={item.product?.images?.[0]?.url}
                       alt={item.product?.title}
